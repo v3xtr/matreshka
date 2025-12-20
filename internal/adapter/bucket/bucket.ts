312f75934127ff646a7b3c5e459e0e6b666b5a0d
v3xtr <j75yhciamoc@gmail.com>
2025-12-20 13:41:39 +0100

    AWS region is now in .env

--- internal/adapter/bucket/bucket.ts
+++ internal/adapter/bucket/bucket.ts
@@ -1,7 +1,7 @@
 import { S3 } from '@aws-sdk/client-s3'
 
 export const s3 = new S3({
-    region: "ru-central-1",
+    region: process.env.AWS_BUCKET_REGION as string,
     endpoint: process.env.AWS_BUCKET_URL as string,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY as string,
