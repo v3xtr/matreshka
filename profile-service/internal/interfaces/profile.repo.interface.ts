7638e925c57449063718374b9083ddb602f902f5
Nikita Kuznetsov <149796739+v3xtr@users.noreply.github.com>
2025-12-21 09:37:18 +0100

    Delete profile-service directory

--- profile-service/internal/interfaces/profile.repo.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateProfileRequest } from "#internal/validation/profile.validation.js";
-import { User } from "src/prisma/index.js";
-
-export interface IProfileRepo{
-    get(id: string): Promise<User | null>
-    create(data: User): Promise<User>
-    update(user: UpdateProfileRequest): Promise<User>
-}
