09357b090e9cd98a430c06df1e6532e89ac70ffe
Nikita Kuznetsov <149796739+v3xtr@users.noreply.github.com>
2025-12-21 09:37:12 +0100

    Delete auth-service directory

--- auth-service/delivery/http/interfaces/auth.controller.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express"
-
-export interface IAuthController{
-    register(req: Request, res: Response): Promise<Response>
-    login(req: Request, res: Response): Promise<Response>
-}
--- auth-service/internal/interfaces/auth.service.interface.ts
+++
@@ -1,8 +0,0 @@
-import { LoginSchemaRequest, RegisterSchemaRequest } from "#internal/validation/auth.validation.js";
-import { RegisterResponseDTO } from "#internal/dto/auth/register.auth.dto.js";
-import { LoginResponseDTO } from "#internal/dto/auth/login.auth.dto.js";
-
-export interface IAuthService{
-    register(data: RegisterSchemaRequest): Promise<RegisterResponseDTO>
-    login(data: LoginSchemaRequest): Promise<LoginResponseDTO>
-}
