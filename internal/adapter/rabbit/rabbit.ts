0a363447ba269d76397b2ec780a469a3a4ef9461
v3xtr <j75yhciamoc@gmail.com>
2025-12-21 17:14:52 +0100

    changed rabbitmq channel

--- internal/adapter/rabbit/rabbit.ts
+++ internal/adapter/rabbit/rabbit.ts
@@ -1,4 +1,4 @@
 import amqplib, { Channel, ChannelModel } from 'amqplib';
 
 const connection: ChannelModel = await amqplib.connect('amqp://localhost');
-export const channel: Channel = await connection.createChannel();
+export const channel: Channel = await connection.createConfirmChannel();
