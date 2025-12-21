2400ee0e3ae20177217971eb1dac8153771c8831
v3xtr <j75yhciamoc@gmail.com>
2025-12-21 08:36:02 +0100

    fixed import

--- internal/adapter/prisma/prisma.ts
+++ internal/adapter/prisma/prisma.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
-import { PrismaClient } from '../../../src/prisma/index.js'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from 'src/prisma/index.js'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is missing')
