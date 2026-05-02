70686e332645bcfdc907f2d6c05ff9ce9d78827a
V3xtr <j75yhciamoc@gmail.com>
2026-05-02 15:19:06 +0200

    initial commit

---
+++ src/test/java/com/matreshka/admin_service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matreshka.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
