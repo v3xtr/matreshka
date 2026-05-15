305ecc770dc361dc3a05bf14c880f22f0bfe8621
V3xtr <j75yhciamoc@gmail.com>
2026-05-15 06:05:41 +0200

    removed enter

--- src/main/java/com/matreshka/media_service/delivery/broker/BrokerConsumer.java
+++ src/main/java/com/matreshka/media_service/delivery/broker/BrokerConsumer.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
 import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
