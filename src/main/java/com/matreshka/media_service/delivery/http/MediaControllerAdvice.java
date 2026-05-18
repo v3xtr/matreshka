fcd0ad22254bbbe5705e2aa1c84812e653daf58d
V3xtr <j75yhciamoc@gmail.com>
2026-05-18 20:21:25 +0200

    changed internal server error message

--- src/main/java/com/matreshka/media_service/delivery/http/MediaControllerAdvice.java
+++ src/main/java/com/matreshka/media_service/delivery/http/MediaControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.matreshka.media_service.delivery.http;
 
-import com.matreshka.media_service.delivery.http.dto.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MediaControllerAdvice {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDTO> handleInternalServerError(Exception e){
-        ErrorResponseDTO errorResponse = new ErrorResponseDTO(e.getMessage(), e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    public ResponseEntity<String> handleInternalServerError(){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Внутряняя ошибка сервера");
     }
 }
