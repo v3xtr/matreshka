1d9d4db8231db974a7dcc33621e167119dcd54ea
V3xtr <j75yhciamoc@gmail.com>
2026-06-08 13:16:09 +0200

    renamed

--- src/main/java/com/matreshka/feed_service/delivery/http/VideoController.java
+++ src/main/java/com/matreshka/feed_service/delivery/http/VideoController.java
@@ -20,7 +20,7 @@ public class VideoController {
 
     private final IVideoService videoService;
 
-    @PostMapping("/addView")
+    @PostMapping("/add-view")
     public ResponseEntity<String> addView(@Valid @RequestBody VideoRequestDTO videoRequestDTO){
         videoService.addView(videoRequestDTO);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
