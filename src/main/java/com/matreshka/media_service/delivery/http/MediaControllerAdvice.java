d0e97535111be5ee46e914549c6d793efc0d750e
V3xtr <j75yhciamoc@gmail.com>
2026-06-29 13:14:02 +0200

    added logs to MediaControllerAdvice

--- src/main/java/com/matreshka/media_service/delivery/http/MediaControllerAdvice.java
+++ src/main/java/com/matreshka/media_service/delivery/http/MediaControllerAdvice.java
@@ -1,14 +1,17 @@
 package com.matreshka.media_service.delivery.http;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class MediaControllerAdvice {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleInternalServerError() {
+    public ResponseEntity<String> handleInternalServerError(Exception e) {
+        log.error("Внутряняя ошибка сервера: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Внутряняя ошибка сервера");
     }
 }
